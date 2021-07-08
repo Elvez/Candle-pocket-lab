5f87e97df39c9883ea1643dd939f547b94447be4
Pravesh Narayan <elvez.brachonzei@gmail.com>
2021-07-08 18:26:28 +0530

    Minor changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,3 @@
-import 'package:candle_pocketlab/Device/connectScreen.dart';
-import 'package:candle_pocketlab/HomeScreen/homescreen.dart';
 import 'package:candle_pocketlab/HomeScreen/loginScreen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
